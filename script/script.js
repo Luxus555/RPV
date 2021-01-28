89ad7d3ab1e8a9403e643ec222f8c379f0e694cf
Luxus555 <luxus555@gmail.com>
2021-01-28 06:52:34 +0000

    Vtoroj urok

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 1500,
+  income = 500,
+  addExpenses = "Аренда, Интернет, Газ",
+  deposit = false,
+  mission = 10000,
+  period = 12,
+  budgetDay = money / 30;
 
-alert("Салют!");
-
-console.log("Превед медвед!");
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " фунтов");
+console.log(addExpenses.toLowerCase().split(", "));
+console.log(budgetDay);
