11ef02f70e000cba7211a79730fa60c10f8f7b9e
Ruslan Pavlov <luxus555@gmail.com>
2021-02-18 21:01:00 +0000

    3.3 update

--- script/language.js
+++ script/language.js
@@ -1,29 +1,32 @@
 
 
-let lang1 = 'en';
+let lang1 = 'en',
+	arr = [];
 
-if (lang1 == 'ru') {
-    arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+if (lang1 === 'ru') {
+   let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 }
-if (lang1 == 'en') {
-    arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+if (lang1 === 'en') {
+    let arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+	alert(arr);
 }
-alert(arr);
 
 let lang2 = 'ru';
 switch (lang2) {
 	case 'ru':
-		arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+		let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 	break;
 	case 'en':
 		arr = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-	break;
-}
-alert(arr);
+		alert(arr);
+	}
+	
 
 let lang3 = 'ru',
- arr = {
+ day = {
 	'ru':['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
 	'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
 };
-alert(arr[lang3]);
+alert(day[lang3]);
+
+let namePerson;
